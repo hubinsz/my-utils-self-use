0605ab7a6ba12f2f40d339cdfc16752da03bcdcf
ifoxbin@gmail.com <ifoxbin@gmail.com@4cc3a5eb-640f-c17a-7928-74d19ca1fb66>
2011-07-14 15:38:38 +0000

    first commit.

---
+++ poi_demo/src/com/mxy/hb/poi/demo/RefinePrinter.java
@@ -0,0 +1,18 @@
+package com.mxy.hb.poi.demo;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hubin
+ * Date: 2011-7-14
+ * Time: 17:17:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class RefinePrinter {
+
+    @Test
+    public void helloTst(){
+        System.out.println("hello, world!");
+    }
+}
